d92dddf4479fb8ac7cced01c9b2563c66d3f3337
AsukFavour <91762394+AsukFavour@users.noreply.github.com>
2022-07-19 11:51:28 +0100

    Update settings.py

--- Native_Brand/settings.py
+++ Native_Brand/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#4n)nbvr6#q@yyxn-%-u1alfis_09ypvv(f(3(n_dd*o-lyztf'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
